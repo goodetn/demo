843e6b3c413139ed2813416dc390c8c3a4c2cb09
tottran <tottianddona@gmail.com>
2022-10-21 18:48:55 +0700

    add travis

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/demo/',
+  base: process.env.NODE_ENV === "production" ? "/demo/" : "/",
   plugins: [
     vue(),
     vueJsx(),
